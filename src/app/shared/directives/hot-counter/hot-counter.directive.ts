6dffb00b3b4a27535439f50a1c3c47bc21deb705
Óscar Villas Pazos <ovillas@hiberus.com>
2021-11-24 20:02:02 +0100

    Cambio la directiva para usar el fromEvent

--- src/app/shared/directives/hot-counter/hot-counter.directive.ts
+++ src/app/shared/directives/hot-counter/hot-counter.directive.ts
@@ -1,26 +1,43 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: 'button[appHotCounter]'
 })
-export class HotCounterDirective {
+export class HotCounterDirective implements OnInit, OnDestroy {
 
   private totalCount: number = 0;
+  private destroy$ = new Subject<void>();
 
   @HostBinding('style.background-color')
   bgColor: string = '';
 
-  constructor() { }
+  constructor(private ref: ElementRef) { }
 
-  @HostListener('click')
+  // @HostListener('click')
   private addClick() {
     this.totalCount++;
     this.bgColor = this.calculateColor(this.totalCount);
   }
 
+  ngOnInit() {
+    fromEvent(this.ref.nativeElement, 'click')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe($event => {
+        console.log($event);
+        this.totalCount++;
+        this.addClick();
+      })
+  }
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   private calculateColor(totalClicks: number): string {
-    if (totalClicks < 5) {return 'green';}
-    if (totalClicks < 10) {return 'yellow';}
+    if (totalClicks < 5) { return 'green'; }
+    if (totalClicks < 10) { return 'yellow'; }
     return 'red';
   }
 
